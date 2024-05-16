d79c87c5ee45f95f4ab17d8979d2608b6807862e
koostyan <165835474+koostyan@users.noreply.github.com>
2024-05-16 21:32:13 +0300

    Add files via upload

--- zadanie.py
+++ zadanie.py
@@ -7,11 +7,11 @@ def test_function():
 
 test_function()
 
-def test_function(): #вызов функции inner_function вне функции test_function
+
+def test_function():  # вызов функции inner_function вне функции test_function
     def inner_function():
         print("Я в области видимости функции test_function")
 
-    inner_function()
-
 
 test_function()
+inner_function()
